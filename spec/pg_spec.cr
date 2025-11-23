85282710c6471b42b808a64f4922fb7fea2ddf7e
Jamie Gaskins <jgaskins@hey.com>
2025-11-23 00:41:06 -0500

    Improve Postgres integration specs

--- spec/pg_spec.cr
+++ spec/pg_spec.cr
@@ -8,6 +8,7 @@ pg = DB.open("postgres:///")
 
 describe "Postgres integration" do
   it "converts a Duration to a Postgres INTERVAL type and back" do
+    # Postgres only has µs precision, so don't test more granularly than that
     duration = Duration.new(months: 3, days: 10, microseconds: 1)
 
     queried = pg.query_one <<-SQL, [duration.to_postgres], as: Duration
@@ -17,4 +18,24 @@ describe "Postgres integration" do
 
     queried.should eq duration
   end
+
+  it "handles being used in DB::Serializable" do
+    # See the `Subscription` struct definition below
+    subscription = pg.query_one <<-SQL, as: Subscription
+      SELECT
+        gen_random_uuid() AS id,
+        '1 month'::interval AS billing_interval,
+        now() AS created_at
+      SQL
+
+    subscription.billing_interval.should eq 1.calendar_month
+  end
+end
+
+struct Subscription
+  include DB::Serializable
+
+  getter id : UUID
+  getter billing_interval : Duration
+  getter created_at : Time
 end
